98499cb41b9da1f72e7d776d87999d21bd20e126
NinaSUEN <tcsuen1@sheffield.ac.uk>
2017-12-07 13:50:29 +0000

    Upload the waggleDance method part

--- Assignment3.java
+++ Assignment3.java
@@ -4,6 +4,26 @@ Written by: Bryn Berkeley, Panagiotis Antoniou, Tsz Ching Suen
 import ShefRobot.*;
 
 public class Assignment3{
+	
+	public static Robot myRobot = new Robot();
+	
+	public static ColorSensor sensor = myRobot.getColorSensor(Sensor.Port.S2);
+        public static Motor leftMotor = myRobot.getLargeMotor(Motor.Port.B);
+        public static Motor rightMotor = myRobot.getLargeMotor(Motor.Port.C);
+	
+	// @author Panagiotis Antoniou
+	public static void waggleDance(){ 
+               for (int t=0; t<10; t++){
+			leftMotor.setSpeed(150);
+			rightMotor.setSpeed(150);
+			leftMotor.forward();
+			rightMotor.backward();
+			myRobot.sleep(t+1*1000);
+			rightMotor.forward();
+			leftMotor.backward();
+			myRobot.sleep(t+1*1000);
+		}
+	   }
 
 	public static void main(String args[]){
 		Robot myRobot = new Robot();
@@ -42,6 +62,9 @@ public class Assignment3{
 		rightMotor.stop();
 		
 		System.out.println(sensor.getColor());
+		System.out.println(sensor.getAmbient());
+				       
+	        waggleDance();
 		myRobot.close();
 	}
 	
